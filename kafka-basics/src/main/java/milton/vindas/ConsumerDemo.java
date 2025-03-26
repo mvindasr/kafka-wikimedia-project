18cf8c39ed4a1859064e404aa25566432c71be3d
mvindasr <mvindasr@ucenfotec.ac.cr>
2025-03-26 08:32:21 -0600

    add consumer and docker compose files

---
+++ kafka-basics/src/main/java/milton/vindas/ConsumerDemo.java
@@ -0,0 +1,59 @@
+package milton.vindas;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Consumer!");
+        String groupId = "my-java-application";
+        String topic = "demo_java";
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:19092");
+
+        // create consumer configs
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        //none: if we don't have customer group we fail, earliest means read from beggining, latest reads records from now
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // suscribe to a topic
+        consumer.subscribe(Arrays.asList(topic));
+
+        // poll for data
+        while (true) {
+            log.info("Polling for data...");
+            ConsumerRecords<String, String> records =
+                consumer.poll(Duration.ofMillis(1000));
+
+            for (ConsumerRecord<String, String> record: records) {
+                log.info("Key: {} | Value: {}", record.key(), record.value());
+                log.info("Partition: {} | Offset: {}", record.partition(), record.offset());
+            }
+        }
+
+
+
+
+    }
+}
